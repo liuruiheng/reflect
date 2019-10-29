4edff8d4a74b6d10a4303cc28b410a3af3dbde0b
DESKTOP-0GUVH1N\19353 <1935308684@qq.com>
2019-10-29 21:53:14 +0800

    reflect

---
+++ src/reflect/Example_01.java
@@ -0,0 +1,30 @@
+package reflect;
+
+public class Example_01 {
+
+	String string;
+	int i,i2,i3;
+	public Example_01() {
+	}
+	public Example_01(String string, int i) {
+		this.string = string;
+		this.i = i;
+	}
+	public Example_01(String... string) {
+		if (0 < string.length) {
+			i = Integer.valueOf(string[0]);
+		}
+		if (1 < string.length) {
+			i2 = Integer.valueOf(string[1]);
+		}
+		if (2 < string.length) {
+			i3 = Integer.valueOf(string[2]);
+		}
+	}
+	public void print() {
+		System.out.println("s="+string);
+		System.out.println("i="+i);
+		System.out.println("i2="+i2);
+		System.out.println("i3="+i3);
+	}
+}
